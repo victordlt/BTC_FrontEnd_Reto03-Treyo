db4e03a6eae232412fe61cec178af69871e94572
victordlt <v.delatorreda@gmail.com>
2022-01-12 21:43:17 +0100

    Cambios Board

--- src/componentes/Board.jsx
+++ src/componentes/Board.jsx
@@ -1,17 +1,29 @@
 import React, {Component} from "react";
+
+import {connect} from 'react-redux';
+
 import Lista from './Lista';
 
 class Board extends Component {
     constructor(props){
         super(props);
-        this.nombrelistas="Tarjeta";
+        this.nombrelista="";
     }
     render(){     
         return(      
-            <div id="colu" class="row" style={{marginTop:1 +'rem'}}>    
-                <Lista></Lista>              
+            <div id="colu" class="row" style={{marginTop:1 +'rem'}}> 
+                {(this.props.listas.map((lista)=>(
+                    <Lista nombre={lista.titulo} id={lista.id}></Lista>           
+                    )))}
             </div>
         )
     }
 }
-export default Board;
+
+const mapStateToProps = (state) =>  ({
+    listas:state.listas,
+    
+});
+
+const connected = connect(mapStateToProps)(Board)
+export default connected;
