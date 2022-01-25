401a9f7a19827e5355b303d8c959e28965c8d8c6
victordlt <v.delatorreda@gmail.com>
2022-01-25 21:41:54 +0100

    App cambios

--- src/App.js
+++ src/App.js
@@ -10,46 +10,27 @@ import 'bootstrap/dist/js/bootstrap.js';
 import Board from './componentes/Board';
 import Barmenu from './componentes/Barmenu';
 import Header from './componentes/Header';
+import Popuplistanueva from './componentes/Popuplistanueva';
+import Popuptareanueva from './componentes/Popuptareanueva';
+import Popupborrarlista from './componentes/Popupborrarlista';
+
+import React from 'react';
 
 
 function App() {
   return (
-    <div className="App">
-     
+    <div className="App">    
         <div class="container">
           <Header/>
           <Barmenu/>
           <Board/>
         </div>
-
-
-        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
-          <div class="modal-content">
-            <div class="modal-header">
-              <input type="text" class="form-control modal-title" placeholder="Introduzca un titulo para la tarea" id="exampleModalLabel"></input>
-              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-              <form>
-                <div class="mb-3">
-                  <label for="message-text" class="col-form-label">Descripción:</label>
-                  <textarea class="form-control" placeholder="Introduzca descripción de la tarea" id="message-text"></textarea>
-                </div>
-              </form>
-            </div>
-            <div class="modal-footer">
-              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
-              <button type="button" class="btn btn-primary">Hecho</button>
-            </div>
-          </div>
-        </div>
-      </div>
-
-
-
+        <Popuplistanueva/>
+        <Popuptareanueva/>
+        <Popupborrarlista/>
     </div>
   );
 }
+
 
 export default App;
